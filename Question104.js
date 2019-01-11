7aaf98679b86a7af0361ea0dd0e63a86012a1eb2
Xueqin Zhang <xueqin@ece.ubc.ca>
2019-01-10 21:45:23 -0800

    question104

---
+++ Question104.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(root == null) return 0;
+    let leftHeight = maxDepth(root.left);
+    let rightHeight = maxDepth(root.right);
+    return Math.max(leftHeight, rightHeight)+1;
+};
