d7d35f3e58373ad4a645e28ca4cd06cc7627d826
Xueqin Zhang <xueqin@ece.ubc.ca>
2019-01-16 22:15:17 -0800

    add java framework

---
+++ java/src/com/abbie/Main.java
@@ -0,0 +1,8 @@
+package com.abbie;
+
+public class Main {
+    public static void main(String[] args) {
+        Question1 question1 = new Question1();
+        question1.twoSum(null, 0);
+    }
+}
---
+++ java/src/com/abbie/Question1.java
@@ -0,0 +1,8 @@
+package com.abbie;
+
+public class Question1 {
+    public int[] twoSum(int[] nums, int target) {
+        System.out.println("Hello Word");
+        return null;
+    }
+}
