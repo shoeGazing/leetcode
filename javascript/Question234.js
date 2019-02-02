f40f91eb7f542164de53992fc9df4b2ccb3089cb
Xueqin Zhang <xueqin@ece.ubc.ca>
2019-02-02 13:04:24 -0800

    question234

---
+++ javascript/Question234.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let fast = head;
+    let slow = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    if(fast){
+        slow = slow.next;
+    }
+    let prev = null;
+    while(slow){
+       let next = slow.next;
+       slow.next = prev;
+       prev = slow;
+       slow = next;
+    }
+    fast = head;
+    while(prev){
+        if(prev.val !== fast.val) return false;
+        prev = prev.next;
+        fast = fast.next;
+    }
+    return true;
+};
